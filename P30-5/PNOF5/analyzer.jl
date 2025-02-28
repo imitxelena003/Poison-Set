a1ef2d4880bcc2c475501f5ab529471ea16d6b42
Juan Felipe Huan Lew Yee <Juan Felipe Huan Lew Yee>
2025-02-28 15:32:07 +0100

    Updated analyzer to look energies: - First the exact file name in P30-5, P30-10, P30-20 - If not found:     Looks the energy in all the files of the same set.
    For example, if MB16-43_21_MgH2.out is not available, use the energy from MB16-43_15_MgH2.out

--- P30-5/PNOF5/analyzer.jl
+++ P30-5/PNOF5/analyzer.jl
@@ -5,70 +5,79 @@ using Statistics
 wd = split(pwd(), "/")
 nof = wd[end]
 superset = wd[end-1]
-######## Change this
+
 data = YAML.load_file("Reactions.yaml")
 data = sort(collect(pairs(data)))
 sets = YAML.load_file("Sets.yaml")
 natoms = parse(Int, split(superset, "-")[2])
-#set_names = collect(keys(data))
-########
 
-function get_nof_fromfile(setname,nof,filename)
-    Emol=0
-    fileparts = split(pwd(), "/")
-    dir = ""
-    for part in fileparts[2:end-2]
-        dir = dir*"/"*part
-    end
-    dir = dir*"/"*setname*"/"*nof*"/"*filename[1:end-4] * ".out"
-    open(dir, "r") do fmol
-        for linemol in readlines(fmol)
-            if occursin("Final NOF", linemol)
-                Emol = parse(Float64, split(linemol)[6])
-            end
-        end
-    end
-    return Emol
+function build_path(rootdir, setname, nof, filename)
+    # Construct the path to the output file.
+    # The path is constructed by joining the root directory, set name, nof, and filename.
+
+    path = joinpath("/", rootdir, setname, nof, filename * ".out")
+    return path
 end
 
-function get_nof_E(nof,filename)
-    Emol = 0
-
-    open(nof * ".dat", "r") do fmol
-        for linemol in readlines(fmol)
-            if filename[1:end-4] == split(linemol)[1]
-                Emol = parse(Float64, split(linemol)[2])
-            end
-        end
-    end
-    if Emol != 0
-        return Emol
-    end
+function get_nof_fromfile(file)
+    # Try to open the file and get the NOF ENergy. If it does not success, return 0.
 
     try
-	Emol = get_nof_fromfile("P30-20",nof,filename)
+        Emol = 0
+        open(file, "r") do fmol
+            for linemol in readlines(fmol)
+                if occursin("Final NOF", linemol)
+                    Emol = parse(Float64, split(linemol)[6])
+                end
+            end
+        end
+        return Emol
     catch
-        try
-	    Emol = get_nof_fromfile("P30-10",nof,filename)
-	catch
-            try
-	        Emol = get_nof_fromfile("P30-5",nof,filename)
-	    catch
-                println(
-                    filename[1:end-4] *
-                    " is not in " *
-                    nof *
-                    ".dat and " *
-                    filename[1:end-4] *
-                    ".out does not exist",
-                    )
-	    end
+        return 0
+    end
+end
+
+function get_nof_E(nof, filename)
+    # Try to get the NOF energy.
+    # First, it looks the exact filename in P30-5, P30-10, and P30-20.
+    # If it does not find, it looks the molecule in the other reactions within the same set.
+
+    # Root directory. First part is empty. Last two parts are set and nof
+    fileparts = split(pwd(), "/")[2:end-2]
+    rootdir = join(fileparts, "/")
+
+    # Look exact filename in all sets
+    for setname in ["P30-5", "P30-10", "P30-20"]
+        dir = build_path(rootdir, setname, nof, filename[1:end-4]) # end-4 to remove xyz
+        Emol = get_nof_fromfile(dir)
+        if Emol < 0
+            return Emol
         end
     end
 
-    return Emol
+    # look the molecule in all reactions within the same set
+    # only molecules start with capital letter
+    molparts = split(filename[1:end-4], "_")
+    if 'A' <= molparts[1][1] <= 'Z'
+        for setname in ["P30-5", "P30-10", "P30-20"]
+            for i in 1:200
+                molparts[2] = string(i)
+                mol = join(molparts, "_")
+                dir = build_path(rootdir, setname, nof, mol)
+                Emol = get_nof_fromfile(dir)
+                if Emol < 0
+                    println(filename[1:end-4], " not found, using ", mol, " in ", setname)
+                    return Emol
+                end
+            end
+        end
+    end
+
+    println("Energy not found:", filename)
+    return 0
 end
 
+# Print Sets and its descriptions
 results = Dict()
 for (name, reaction) in data
     set_name, system_name = split(name, ":")
@@ -79,6 +88,19 @@ for (set_name, set_description) in results
     results[set_name] = Dict()
 end
 
+# Build structure for YAML file.
+####################################################################
+# - Sets: Each set has many systems (reactions) labeled by a number
+#   Each system has:
+#     - dE_Ref: Reaction Reference energy
+#     - dE_NOF: Reaction Reference energy
+#     - AD: Absolute Deviation
+#     - APD: Absolute Percentage Deviation
+#     - Species: Molecules
+#       Each molecule has:
+#         - E_NOF: NOF Energy
+#         - Count: Stechiometry in reaction
+####################################################################
 for (reaction_name, reaction) in data
     set_name, system_name = split(reaction_name, ":")
     dE = 0.0
@@ -106,6 +128,10 @@ for (reaction_name, reaction) in data
     system["APD"] = AD
     results[set_name][system_name] = system
 end
+
+YAML.write_file(superset * "-" * nof * ".yaml", results)
+
+#### Print results in terminal (just for visualization of data)
 
 MADs = Dict()
 MAPDs = Dict()
@@ -135,171 +161,32 @@ for (set_name, set) in results
     MAPDs[set_name] = mean(APDs)
 end
 
-YAML.write_file(superset * "-" * nof * ".yaml", results)
+# For plotting
 
 using Plots
 
-cg = cgrad(:RdYlGn, rev = true)
-colors = get.(Ref(cg), (values(MADs) .- minimum(values(MADs))) ./ maximum(values(MADs)))
-set_names = collect(keys(MADs))
-p = bar(
-    collect(keys(MADs)),
-    collect(values(MADs)),
-    title = nof,
-    ylabel = "Mean Abs. Dev. (kcal/mol)",
-    ylims = (0, 40),
-    xrotation = 90,
-    xticks = (0.5:1:size(set_names)[1], keys(MADs)),
-    color = colors,
-    legend = false,
-)
-display(p)
+function plot_bar(data, ylabel, ylims)
+    cg = cgrad(:RdYlGn, rev=true)
+    colors = get.(Ref(cg), (values(data) .- minimum(values(data))) ./ maximum(values(data)))
+    set_names = collect(keys(data))
+    p = bar(
+        set_names,
+        collect(values(data)),
+        title=nof,
+        ylabel=ylabel,
+        ylims=ylims,
+        xrotation=90,
+        xticks=(0.5:1:size(set_names)[1], set_names),
+        color=colors,
+        legend=false,
+    )
+    display(p)
+end
+
+plot_bar(MADs, "Mean Abs. Dev. (kcal/mol)", (0, 40))
 println("Press the enter key to quit:")
 readline()
 
-cg = cgrad(:RdYlGn, rev = true)
-colors = get.(Ref(cg), (values(MAPDs) .- minimum(values(MAPDs))) ./ maximum(values(MAPDs)))
-p = bar(
-    collect(keys(MAPDs)),
-    collect(values(MAPDs)),
-    title = nof,
-    ylabel = "MAPD (%)",
-    ylims = (0, 100),
-    xrotation = 90,
-    xticks = (0.5:1:size(set_names)[1]-0.5, keys(MAPDs)),
-    color = colors,
-    legend = false,
-)
-display(p)
+plot_bar(MAPDs, "MAPD (%)", (0, 100))
 println("Press the enter key to quit:")
 readline()
-
-
-#
-#
-##WTMAD2 = 0
-#set_name_old = nothing
-#for (name, system) in data
-#    set_name, system_name = split(name, ":")
-#    if set_name != set_name_old
-#        println("--------------------------------------")
-#        println(set_name)
-#        println("--------------------------------------")
-#        global set_name_old = set_name
-#    end
-#    dE = 0
-#    dE_NOF = 0
-#    for info in reaction
-#        if typeof(info) <: AbstractFloat
-#            dE = info
-#            println("---------------")
-#            @printf("  %s dE_ref: %.4f \n", system_name, dE)
-#            println("---------------")
-#        else
-#            count, filename = info
-#            E_NOF = get_nof_E(filename)
-#            dE_NOF += count * E_NOF
-#            @printf("    %-20s %3d %10.4f \n", filename[1:end-4], count, E_NOF)
-#        end
-#    end
-#    AD = abs(dE - dE_NOF * 627.15)
-#    @printf("  dE NOF: %.4f   AD: %.1f\n", dE_NOF * 627.15, AD)#, system["Weight"], system["Weight"]*AD/30)
-#end
-##WTMAD2 = WTMAD2/nsystems
-##@printf("WTMAD2 = %.1f\n", WTMAD2)
-##
-#using Plots
-#
-#MADs = Dict()
-#set_name_old = nothing
-#ADs = []
-#for (name, reaction) in data
-#    set_name, system_name = split(name, ":")
-#    if set_name != set_name_old
-#        if (set_name_old != nothing)
-#            MADs[set_name_old] = mean(ADs)
-#        end
-#        global ADs = []
-#    end
-#    global set_name_old = set_name
-#    dE = 0
-#    dE_NOF = 0
-#    for info in reaction
-#        if typeof(info) <: AbstractFloat
-#            dE = info
-#        else
-#            count, filename = info
-#            E_NOF = get_nof_E(filename)
-#            dE_NOF += count * E_NOF
-#        end
-#    end
-#    AD = abs(dE - dE_NOF * 627.15)
-#    push!(ADs, AD)
-#end
-#MADs[set_name_old] = mean(ADs)
-#println(MADs)
-#
-#
-#cg = cgrad(:RdYlGn, rev = true)
-#colors = get.(Ref(cg), (values(MADs) .- minimum(values(MADs))) ./ maximum(values(MADs)))
-#set_names = collect(keys(MADs))
-#p = bar(
-#    collect(keys(MADs)),
-#    collect(values(MADs)),
-#    title = nof,
-#    ylabel = "Mean Abs. Dev. (kcal/mol)",
-#    ylims = (0, 40),
-#    xrotation = 90,
-#    xticks = (0.5:1:size(set_names)[1], keys(MADs)),
-#    color = colors,
-#    legend = false,
-#)
-#display(p)
-#println("Press the enter key to quit:")
-#readline()
-#
-#MAPDs = Dict()
-#set_name_old = nothing
-#APDs = []
-#for (name, reaction) in data
-#    set_name, system_name = split(name, ":")
-#    if set_name != set_name_old
-#        if (set_name_old != nothing)
-#            MAPDs[set_name_old] = mean(APDs)
-#        end
-#        global APDs = []
-#    end
-#    global set_name_old = set_name
-#    dE = 0
-#    dE_NOF = 0
-#    for info in reaction
-#        if typeof(info) <: AbstractFloat
-#            dE = info
-#        else
-#            count, filename = info
-#            E_NOF = get_nof_E(filename)
-#            dE_NOF += count * E_NOF
-#        end
-#    end
-#    APD = abs(dE - dE_NOF * 627.15) / dE * 100
-#    push!(APDs, APD)
-#end
-#MAPDs[set_name_old] = mean(APDs)
-#println(MAPDs)
-#
-#cg = cgrad(:RdYlGn, rev = true)
-#colors = get.(Ref(cg), (values(MAPDs) .- minimum(values(MAPDs))) ./ maximum(values(MAPDs)))
-#p = bar(
-#    collect(keys(MAPDs)),
-#    collect(values(MAPDs)),
-#    title = nof,
-#    ylabel = "MAPD (%)",
-#    ylims = (0, 100),
-#    xrotation = 90,
-#    xticks = (0.5:1:size(set_names)[1]-0.5, keys(MAPDs)),
-#    color = colors,
-#    legend = false,
-#)
-#display(p)
-#println("Press the enter key to quit:")
-#readline()
